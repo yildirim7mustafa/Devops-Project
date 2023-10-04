2963ded92895f65ed40a3a2442f70f7e71de483d
yildirim <yildirim7mustafa@gmail.com>
2023-10-04 12:21:59 +0300

    argocd testv4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 	@GetMapping
-	public String message() {return "MUTİ"; }
+	public String message() {return "MUTİİ"; }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
