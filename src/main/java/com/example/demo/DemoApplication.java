ed346ae3dea5979239da0b91c082b5a3797141ba
yildirim7mustafa <yildirim7mustafa@gmail.com>
2023-10-17 16:56:23 +0300

    v3.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 	@GetMapping
-	public String message() {return "Lorem v2"; }
+	public String message() {return "Lorem v3"; }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
