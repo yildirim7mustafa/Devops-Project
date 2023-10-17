617520d3dd0a88da888a9ecb223867488659089f
yildirim7mustafa <yildirim7mustafa@gmail.com>
2023-10-17 13:50:00 +0300

    back to old commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 	@GetMapping
-	public String message() {return "TONNY MONTANA ."; }
+	public String message() {return "HE HE HE HENESSY & ATI242 DIGI A"; }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
